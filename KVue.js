555f317e51ebf87df1c6c0aded479f629e2d142c
卿佳文 <qingjiawen@ip-192-168-43-14.ap-northeast-1.compute.internal>
2020-08-03 07:32:16 +0800

    修改

--- KVue.js
+++ KVue.js
@@ -12,6 +12,11 @@ class KVue {
 
     this.observe(this.$data);
 
+    new Watcher(this, 'foo');
+    this.foo; //读一次，触发依赖收集
+    new Watcher(this, 'bar.mua');
+    this.bar.mua; //读一次，触发依赖收集
+
   }
 
   observe(value) {
@@ -44,19 +49,59 @@ class KVue {
   defineReactive(obj, key, val) {
     //遍历递归
     this.observe(val);
+
+    //定义了一个Dep
+    const dep = new Dep(); //每个dep实例和data中每个Key有一对一关系
+
+
     //给obj的每一个key定义拦截
 
     Object.defineProperty(obj, key, {
       get() {
+        //依赖收集
+        Dep.target && dep.addDep(Dep.target);
         return val;
       },
       set(newVal) {
         if (newVal !== val) {
           val = newVal;
-          console.log(key + '属性更新了');
+          // console.log(key + '属性更新了');
+          dep.notify();
         }
       }
     })
   }
 
+}
+
+//创建Dep：管理所有Watcher
+
+class Dep {
+  constructor() {
+    //存储所有依赖
+    this.watchers = []
+  }
+
+  addDep(watcher) {
+    this.watchers.push(watcher);
+  }
+
+  notify() {
+    this.watchers.forEach(watcher => watcher.update())
+  }
+}
+
+//创建Watcher:保存data中数值和页面中的挂钩关系
+class Watcher {
+  constructor(vm, key) {
+    //创建实例时立刻将该实例指向Dep.target便于依赖收集
+    Dep.target = this;
+    this.vm = vm;
+    this.key = key;
+  }
+
+  //更新
+  update() {
+    console.log(this.key + "更新了！");
+  }
 }
